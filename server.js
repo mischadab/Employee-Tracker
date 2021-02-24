0a58226f0e029e66c35d118f6be919b203093973
mischadab <mischadabrowski@gmail.com>
2021-02-24 13:39:23 -0500

    start inquirer prompt function added

--- server.js
+++ server.js
@@ -3,3 +3,30 @@ const inquirer = require('inquirer')
 const mysql = require('mysql')
 const cTable = require('console.table')
 const db = require( './app/connection' )('employees','mischadab')
+
+
+// function for question prompt
+async function startPrompt(){
+    const answer = await inquirer.prompt([
+        { type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: [ 
+            'View all Employees',
+            'View all Employees by Department',
+            'View all Employees by Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'View all Managers',
+            'Add Manager',
+            'View all Roles',
+            'Remove Role',
+            'View all Departments',
+            'Add Department',
+            'Remove Department',
+        ]
+        }
+    ])
+}
