274e2765f3dfb21c3c592f72d0c652bec3988f50
mischadab <mischadabrowski@gmail.com>
2021-02-26 18:06:50 -0500

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee VALUES(0, 'Mischa', 'Dabrowski', 3, 5);
+INSERT INTO manager VALUES(0, 'Andrew Boyle');
+INSERT INTO role VALUES(0, 'Developer', 100000, 1);
+INSERT INTO department VALUES(0, 'Engineering');
